f4a2d639f1f9abee26711afdef4bdbc5fb0caf62
peopleinfo <49970466+peopleinfo@users.noreply.github.com>
2026-09-03 00:40:55 +0700

    test: use lint-compatible supertest import

--- apps/api/test/health.e2e-spec.ts
+++ apps/api/test/health.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { INestApplication } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
-import request = require('supertest')
+import * as request from 'supertest'
 import { AppModule } from '../src/app.module'
 
 type HealthResponse = {
